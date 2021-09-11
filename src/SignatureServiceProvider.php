408600249d9fa6801d62e54d5ae0a6c1dd9d4f86
Ashraf Monshi <wh7r.com@gmail.com>
2021-09-11 23:06:52 +0300

    wip

--- src/SignatureServiceProvider.php
+++ src/SignatureServiceProvider.php
@@ -18,17 +18,13 @@ class SignatureServiceProvider extends ServiceProvider
         View::composer('*', function ($view) {
             return $view->with('atmStyles','https://atm-code.com/css/atm-sign.css');
         });
-
-        View::composer('*', function ($view) {
-            return $view->with('atmZeus','<span class="font-koho text-gray-700 group"><span class="font-semibold text-green-600 group-hover:text-yellow-500 transition ease-in-out duration-300">Lara&nbsp;<span class="line-through italic text-yellow-500 group-hover:text-green-600 transition ease-in-out duration-300">Z</span>eus</span></span>');
-        });
-
+        
         Blade::directive('atmCode', function () {
             return '<link rel="stylesheet" href="{{ $atmStyles }}"><span class="font-courier"><span class="rounded-lg bg-gray-200 italic px-2 dark:bg-gray-600"><span class="not-italic">ATM ~ </span><span class="not-italic text-purple-500">&lt;?</span><span> code</span><span class="blink-cursor not-italic font-thin text-gray-400">|</span><span class="not-italic text-purple-500">?&gt;</span></span></span>';
         });
 
         Blade::directive('zeus', function ($part = null) {
-            return '{!! $atmZeus !!}'
+            return '<span class="font-koho text-gray-700 group"><span class="font-semibold text-green-600 group-hover:text-yellow-500 transition ease-in-out duration-300">Lara&nbsp;<span class="line-through italic text-yellow-500 group-hover:text-green-600 transition ease-in-out duration-300">Z</span>eus</span></span>'
                 . ($part) ?? '<span class="text-base tracking-wide text-gray-500">{$part}</span>';
         });
 
