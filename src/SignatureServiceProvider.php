4602350a2101392329a79d65a47c4ebc3e541dda
Ashraf Monshi <wh7r.com@gmail.com>
2021-09-11 22:35:49 +0300

    wip

--- src/SignatureServiceProvider.php
+++ src/SignatureServiceProvider.php
@@ -13,16 +13,19 @@ class SignatureServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // I know! 🤷🏽‍, please let me have my fun!!!
+
         View::composer('*', function ($view) {
             return $view->with('atmStyles','https://atm-code.com/css/atm-sign.css');
+        });
+
+        Blade::directive('atmCode', function () {
+            return '<link rel="stylesheet" href="{{ $atmStyles }}"><span class="font-courier"><span class="rounded-lg bg-gray-200 italic px-2 dark:bg-gray-600"><span class="not-italic">ATM ~ </span><span class="not-italic text-purple-500">&lt;?</span><span> code</span><span class="blink-cursor not-italic font-thin text-gray-400">|</span><span class="not-italic text-purple-500">?&gt;</span></span></span>';
         });
 
         Blade::directive('zeus', function ($part = null) {
             return '<span class="text-gray-700 group"><span class="font-semibold text-green-600 group-hover:text-yellow-500 transition ease-in-out duration-300">Lara&nbsp;<span class="line-through italic text-yellow-500 group-hover:text-green-600 transition ease-in-out duration-300">Z</span>eus</span></span>'
                 . ($part) ?? '<span class="text-base tracking-wide text-gray-500">{$part}</span>';
-        });
-        Blade::directive('atmCode', function () {
-            return '<link rel="stylesheet" href="{{ $atmStyles }}"><span class="font-courier"><span class="rounded-lg bg-gray-200 italic px-2 dark:bg-gray-600"><span class="not-italic">ATM ~ </span><span class="not-italic text-purple-500">&lt;?</span><span> code</span><span class="blink-cursor not-italic font-thin text-gray-400">|</span><span class="not-italic text-purple-500">?&gt;</span></span></span>';
         });
     }
 
