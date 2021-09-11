1edb5585c798c6de35a70ac8ddbca8d84fb5ed8e
Ashraf Monshi <wh7r.com@gmail.com>
2021-09-12 01:26:58 +0300

    wip

--- src/SignatureServiceProvider.php
+++ src/SignatureServiceProvider.php
@@ -20,12 +20,12 @@ class SignatureServiceProvider extends ServiceProvider
         });
 
         Blade::directive('atmCode', function () {
-            return '<link rel="stylesheet" href="{{ $atmStyles }}"><span class="font-courier"><span class="rounded-lg bg-gray-200 italic px-2 dark:bg-gray-600"><span class="not-italic">ATM ~ </span><span class="not-italic text-purple-500">&lt;?</span><span> code</span><span class="blink-cursor not-italic font-thin text-gray-400">|</span><span class="not-italic text-purple-500">?&gt;</span></span></span>';
+            return '<link rel="stylesheet" href="{{ $atmStyles }}"><span class="font-courier"><span class="font-courier rounded-lg bg-gray-200 italic px-2 dark:bg-gray-600"><span class="font-courier not-italic">ATM ~ </span><span class="font-courier not-italic text-purple-500">&lt;?</span><span> code</span><span class="font-courier blink-cursor not-italic font-thin text-gray-400">|</span><span class="font-courier not-italic text-purple-500">?&gt;</span></span></span>';
         });
 
         Blade::directive('zeus', function ($part = null) {
-            return '<span class="font-koho text-gray-700 group"><span class="font-semibold text-green-600 group-hover:text-yellow-500 transition ease-in-out duration-300">Lara&nbsp;<span class="line-through italic text-yellow-500 group-hover:text-green-600 transition ease-in-out duration-300">Z</span>eus</span></span>'
-                . ($part) ?? '<span class="text-base tracking-wide text-gray-500">{$part}</span>';
+            return '<span class="font-koho text-gray-700 group"><span class="font-koho font-semibold text-green-600 group-hover:text-yellow-500 transition ease-in-out duration-300">Lara&nbsp;<span class="font-koho line-through italic text-yellow-500 group-hover:text-green-600 transition ease-in-out duration-300">Z</span>eus</span></span>'
+                . ($part) ?? '<span class="font-koho text-base tracking-wide text-gray-500">{$part}</span>';
         });
 
     }
