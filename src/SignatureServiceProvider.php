f2a2bca0639696618a9486197a9184fcc79e8845
Ashraf Monshi <wh7r.com@gmail.com>
2021-09-24 12:39:57 +0300

    add stats

--- src/SignatureServiceProvider.php
+++ src/SignatureServiceProvider.php
@@ -2,21 +2,18 @@
 
 namespace AtmCode\Signature;
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class SignatureServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application services.
-     */
     public function boot()
     {
         // I know! 🤷🏽‍, please let me have my fun!!!
-
         View::composer('*', function ($view) {
-            return $view->with('atmStyles','https://atm-code.com/css/atm-sign.css');
+            return $view->with('atmStyles', 'https://atm-code.com/css/atm-sign.css');
         });
 
         Blade::directive('atmCode', function () {
@@ -25,19 +22,25 @@ class SignatureServiceProvider extends ServiceProvider
 
         Blade::directive('zeus', function ($part = null) {
             return '<span class="font-koho text-gray-700 group"><span class="font-koho font-semibold text-green-600 group-hover:text-yellow-500 transition ease-in-out duration-300">Lara&nbsp;<span class="font-koho line-through italic text-yellow-500 group-hover:text-green-600 transition ease-in-out duration-300">Z</span>eus</span></span>'
-                . ($part) ?? '<span class="font-koho text-base tracking-wide text-gray-500">{$part}</span>';
+                .($part) ?? '<span class="font-koho text-base tracking-wide text-gray-500">{$part}</span>';
         });
 
+        Blade::directive('atmStats', function ($code) {
+            Artisan::call('view:clear');
+            if (!app()->isLocal() && optional(auth()->user())->email !== 'wh7r.com@gmail.com') {
+                return '<script async defer data-website-id="'.$code.'" src="https://stats.atm-code.com/umami.js"></script>';
+            }
+
+            return '';
+        });
     }
 
-    /**
-     * Register the application services.
-     */
     public function register()
     {
-        // Register the main class to use with the facade
         $this->app->singleton('signature', function () {
             return new signature;
         });
+
+
     }
 }
